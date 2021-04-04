2195d0272d53ae0a3822601c8a336e85f9262d6a
methias <tmn5@pm.me>
2021-04-04 15:01:45 +0300

    Add _data dir

---
+++ src/_data/helpers.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /**
+   * From Andy's course on Eleventy with some changes.
+   * Returns back some attributes based on whether the
+   * link is active or a parent of an active item
+   *
+   * @param {String} itemUrl The link in question
+   * @param {String} pageUrl The page context
+   * @returns {String} The attributes or empty
+   */
+  getLinkActiveState(itemUrl, pageUrl) {
+    let response = ''
+
+    if (itemUrl === pageUrl) {
+      response = ' aria-current="page"'
+    }
+
+    return response
+  },
+}
